307174e886620bda660a00880878566066969813
Tyler Gaw <me@tylergaw.com>
2023-07-11 21:02:33 -0400

    Roughs out schedule styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  inlineStylesheets: "auto",
+  // NOTE: This will be the default in 3.x
+  // See https://github.com/withastro/astro/releases/tag/astro%402.4.0
+  scopedStyleStrategy: "class",
+});
