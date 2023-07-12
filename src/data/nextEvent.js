f2046c714af5319a33826f34e7e0da9c1281dd50
Tyler Gaw <me@tylergaw.com>
2023-07-12 18:56:06 -0400

    Sets tickets live

--- src/data/nextEvent.js
+++ src/data/nextEvent.js
@@ -1,6 +1,6 @@
 export default {
   datetime: "Tuesday, August 15th, 7-9pm",
-  url: null,
+  url: "https://ti.to/css-club-nyc/august-2023",
   location: {
     title: "Resobox East Village",
     addr: "91 E 3rd St, NYC",
