c5a25e6934dbd371449edeb792202d2e42aae495
Tyler Gaw <me@tylergaw.com>
2023-07-12 15:59:41 -0400

    Adds social share details

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ export default defineConfig({
   // NOTE: This will be the default in 3.x
   // See https://github.com/withastro/astro/releases/tag/astro%402.4.0
   scopedStyleStrategy: "class",
+  site: "https://cssclub.nyc",
 });
